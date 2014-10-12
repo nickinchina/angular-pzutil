8a4d2db80e36abc74c851875d3237d77e2b9d92f
gordon <shoutsoft@hotmail.com>
2014-10-12 15:46:53 +0800

    fixed templates

--- dist/pzutil-tpls.js
+++ dist/pzutil-tpls.js
@@ -960,8 +960,8 @@ angular.module("template/simplegrid/header.html", []).run(["$templateCache", fun
     "<div class=\"row sg-gridheader\" >\n" +
     "    <div class=\"{{col.$getColumnClass()}}\" ng-click=\"col.$sort()\" ng-repeat=\"col in columns\">\n" +
     "        <span>{{col.$getTitle()}}</span>\n" +
-    "        <i class=\"fa fa-sort-desc pull-right\" ng-show=\"col.sortOrder\"></i>\n" +
-    "        <i class=\"fa fa-sort-asc pull-right\" ng-show=\"!col.sortOrder && col.sortOrder!=undefined\"></i>\n" +
+    "        <i class=\"fa fa-long-arrow-down pull-right\" ng-show=\"col.sortOrder\"></i>\n" +
+    "        <i class=\"fa fa-long-arrow-up pull-right\" ng-show=\"!col.sortOrder && col.sortOrder!=undefined\"></i>\n" +
     "    </div>\n" +
     "</div>");
 }]);
