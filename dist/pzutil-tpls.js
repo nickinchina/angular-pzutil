bf648eadc9890dc0006981b7332c1d1beb3dd916
gordon <shoutsoft@hotmail.com>
2015-02-18 16:15:01 +0800

    hh

--- dist/pzutil-tpls.js
+++ dist/pzutil-tpls.js
@@ -578,7 +578,79 @@ angular.module('pzutil.simplegrid', ['pzutil.services','pzutil.modal'])
                 $modalInstance.dismiss('cancel');
             };
         }])
-
+    .directive('contextMenu', function ($parse) {
+        var renderContextMenu = function ($scope, event, options) {
+            if (!$) { var $ = angular.element; }
+            $(event.currentTarget).addClass('context');
+            var $contextMenu = $('<div>');
+            $contextMenu.addClass('dropdown clearfix');
+            var $ul = $('<ul>');
+            $ul.addClass('dropdown-menu');
+            $ul.attr({ 'role': 'menu' });
+            $ul.css({
+                display: 'block',
+                position: 'absolute',
+                left: event.pageX + 'px',
+                top: event.pageY + 'px'
+            });
+            angular.forEach(options, function (item, i) {
+                var $li = $('<li>');
+                if (item === null) {
+                    $li.addClass('divider');
+                } else {
+                    $a = $('<a>');
+                    $a.attr({ tabindex: '-1', href: '#' });
+                    $a.text(typeof item[0] == 'string' ? item[0] : item[0].call($scope, $scope));
+                    $li.append($a);
+                    $li.on('click', function ($event) {
+                        $event.preventDefault();
+                        $scope.$apply(function () {
+                            $(event.currentTarget).removeClass('context');
+                            $contextMenu.remove();
+                            item[1].call($scope, $scope);
+                        });
+                    });
+                }
+                $ul.append($li);
+            });
+            $contextMenu.append($ul);
+            var height = Math.max(
+                document.body.scrollHeight, document.documentElement.scrollHeight,
+                document.body.offsetHeight, document.documentElement.offsetHeight,
+                document.body.clientHeight, document.documentElement.clientHeight
+            );
+            $contextMenu.css({
+                width: '100%',
+                height: height + 'px',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                zIndex: 9999
+            });
+            $(document).find('body').append($contextMenu);
+            $contextMenu.on("mousedown", function (e) {
+                if ($(e.target).hasClass('dropdown')) {
+                    $(event.currentTarget).removeClass('context');
+                    $contextMenu.remove();
+                }
+            }).on('contextmenu', function (event) {
+                $(event.currentTarget).removeClass('context');
+                event.preventDefault();
+                $contextMenu.remove();
+            });
+        };
+        return function ($scope, element, attrs) {
+            var options = $scope.$eval(attrs.contextMenu);
+            if (options instanceof Array) {
+                element.on('contextmenu', function (event) {
+                    $scope.$apply(function () {
+                        event.preventDefault();
+                        renderContextMenu($scope, event, options);
+                    });
+                });
+            }
+        };
+    })
     .directive('simpleGridChart', [ function() {
         return {
             restrict:'E',
@@ -647,7 +719,7 @@ angular.module('pzutil.simplegrid', ['pzutil.services','pzutil.modal'])
                 scope: { data:"=sgData", listItems:"=",  sgAddObject:"&", sgSortOptions:"=", itemtemplate:"=sgTemplate",sgColumns:"@",sgDelObject:"&", sgAllowDel:"@",
                     sgNoPager:'=', sgOnClick:'&', sgLookup:"&", sgGlobalSearch:"@", sgLocalSearch:"@",sgPageSize:"@" ,sgOptions:"=", sgOnChange:"&", sgLookupTitle:"&",sgSortField:"=",sgVirtual:"@",
                     sgCheckColumn:"@", sgCustomSearch:"&", sgModalSearchTemplate:"=", sgModalSearchController:"=", sgModalSearchResolve:"=", sgModalSearch:"&", sgExportTitle:"@",
-                    sgPublic:"=", sgAgg:"&", sgReadonly:"="},
+                    sgPublic:"=", sgAgg:"&", sgReadonly:"=", sgMenu:"="},
                 templateUrl: function($element, $attrs) {
                     var t = $attrs.sgTemplate;
                     if (t) {
@@ -1398,7 +1470,7 @@ angular.module("template/simplegrid/simpleGrid-normal.html", []).run(["$template
     "    <ng-include src=\"'template/simplegrid/header.html'\"></ng-include>\n" +
     "    <div>\n" +
     "        <div style=\"{{scrollStyle}}\">\n" +
-    "            <div ng-repeat=\"item in items\" class=\"row sg-gridrow\" ng-click=\"clickRow(item,$event)\" ng-class=\"{true: 'sg-gridrow-active'}[item.$__selected]\" >\n" +
+    "            <div ng-repeat=\"item in items\" class=\"row sg-gridrow\" ng-click=\"clickRow(item,$event)\" ng-class=\"{true: 'sg-gridrow-active'}[item.$__selected]\" context-menu=\"sgMenu\" >\n" +
     "                <div class=\"{{col.$getColumnClass(item)}}\" ng-repeat=\"col in columns\" title=\"{{col.$getText(item)}}\">\n" +
     "                    <i ng-if=\"$first && item.$__selected\" class=\"fa fa-circle\"></i>\n" +
     "                    <i ng-if=\"col.bool\" ng-class=\"{true: 'fa fa-check'}[col.$getValue(item)]\"></i>\n" +
