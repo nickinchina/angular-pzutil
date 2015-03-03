d089251bd6768ba5ff4087c00ac5d4003c0fdaf8
gordon <shoutsoft@hotmail.com>
2015-03-03 14:06:55 +0800

    hh

--- src/simplegrid/simpleGrid.js
+++ src/simplegrid/simpleGrid.js
@@ -16,9 +16,9 @@ angular.module('pzutil.simplegrid', ['pzutil.services','pzutil.modal'])
             var mixin = function (data, idx) {
                 data.checkbox = ($scope.sgCheckColumn == data.name);
                 if (showDel && idx==0) {
+                    console.log(showDel, data.width);
                     data.width=data.width - 0.5;
                 }
-                console.log(showDel, idx);
                 angular.extend(this, data);
             };
             mixin.sorter = sorter;
