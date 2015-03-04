dd5bfe955d6c9431b0ac6ff12577d04f7426840d
gordon <shoutsoft@hotmail.com>
2015-03-04 17:22:17 +0800

    hh

--- dist/pzutil-tpls.js
+++ dist/pzutil-tpls.js
@@ -1559,7 +1559,7 @@ angular.module("template/simplegrid/footer-virtual.html", []).run(["$templateCac
 angular.module("template/simplegrid/footer.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/simplegrid/footer.html",
     "<div class=\"row sg-gridrow\" ng-if=\"hasSummary\">\n" +
-    "    <div class=\"{{col.$getColumnClass(item)}}\" ng-repeat=\"col in columns\">\n" +
+    "    <div class=\"{{col.$getColumnClass({})}}\" ng-repeat=\"col in columns\">\n" +
     "        <span class=\"text-success\">{{col.$aggregate()|picker:col.format}} <i class=\"fa fa-bar-chart sg_gridIcon text-info\" ng-if=\"!!col.chartSeries\" ng-click=\"col.$chart()\" style=\"top:50%;\"></i></span>\n" +
     "    </div>\n" +
     "</div>\n" +
