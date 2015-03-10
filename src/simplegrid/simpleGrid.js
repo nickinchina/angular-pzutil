32d979cd63931043d0889838deb05c4e0164bc24
gordon <shoutsoft@hotmail.com>
2015-03-10 14:19:31 +0800

    hh

--- src/simplegrid/simpleGrid.js
+++ src/simplegrid/simpleGrid.js
@@ -60,10 +60,9 @@ angular.module('pzutil.simplegrid', ['pzutil.services','pzutil.modal'])
                     return checkbox + 'sg-gridrow-cell col-sg-' + w;
             };
             mixin.prototype.$modalEdit = function(item, e){
-                if (this.modalEdit)
+                if (this.modalEdit){
                     modalEditor(item,this, $(e.target));
-                else
-                    clickRow(item,e);
+                }
             };
             mixin.prototype.$getComboKey=function(type){
                 switch (type){
