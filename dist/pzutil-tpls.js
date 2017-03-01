722763935161fc7aa845753c4920d4376d19f114
root <shoutsoft@hotmail.com>
2017-03-01 06:50:59 +0000

    c9

--- dist/pzutil-tpls.js
+++ dist/pzutil-tpls.js
@@ -1749,8 +1749,8 @@ angular.module("template/simplegrid/simpleGrid.html", []).run(["$templateCache",
 
 var sgReact = React.createClass( {displayName: "sgReact",
     propTypes : {
-        items: React.PropTypes.object.isRequired,
-        columns: React.PropTypes.object.isRequired
+        items: React.PropTypes.array.isRequired,
+        columns: React.PropTypes.array.isRequired
     },
 
     getDefaultProps: function() {
@@ -1758,11 +1758,12 @@ var sgReact = React.createClass( {displayName: "sgReact",
     },
 
     render: function() {
+        var self = this;
         return (React.createElement("div", null, 
              this.props.items.map(function(item) {
                     return React.createElement("div", {class: "row sg-gridrow"}, 
                         
-                            this.props.columns.map(function(col){
+                            self.props.columns.map(function(col){
                                 return (
                                 React.createElement("div", {class: "{col.$getColumnClass(item)}", style: "{col.$getColumnStyle()}", title: "{col.$getText(item)}"}, 
                                     "col.$getText(item)"
--- jsx/sgreact.jsx
+++ jsx/sgreact.jsx
@@ -1,7 +1,7 @@
 var sgReact = React.createClass( {
     propTypes : {
-        items: React.PropTypes.object.isRequired,
-        columns: React.PropTypes.object.isRequired
+        items: React.PropTypes.array.isRequired,
+        columns: React.PropTypes.array.isRequired
     },
 
     getDefaultProps: function() {
@@ -9,11 +9,12 @@ var sgReact = React.createClass( {
     },
 
     render: function() {
+        var self = this;
         return (<div>
             { this.props.items.map(function(item) {
                     return <div class="row sg-gridrow">
                         {
-                            this.props.columns.map(function(col){
+                            self.props.columns.map(function(col){
                                 return (
                                 <div class="{col.$getColumnClass(item)}" style="{col.$getColumnStyle()}" title="{col.$getText(item)}">
                                     col.$getText(item)
